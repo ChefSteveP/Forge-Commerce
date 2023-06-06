fc538061735ad7bcbb709600cdb66e1bc37ae195
Jacob Wald <vug5ue@virginia.edu>
2023-06-06 10:06:19 -0400

    Route Locations

---
+++ frontend/src/app/RouteLocations.js
@@ -0,0 +1,13 @@
+export const RouteLocations = {
+  // add your routes here
+
+  login: "/login",
+  createAccount: "/createAccount",
+  discovery: "/discovery",
+  forums: "/forums",
+  profile: "/profile",
+  topArtists: "/profile/topArtists",
+  topSongs: "/profile/topSongs",
+  likedSongs: "/profile/liked-songs",
+  chat: "/discovery/chat",
+};
