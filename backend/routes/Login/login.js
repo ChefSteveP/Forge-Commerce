d429cc2d882f929c042135151a872316bbb70a26
simonanderson16 <uhx8bu@virginia.edu>
2023-06-06 21:26:34 -0400

    route to add user

--- backend/routes/Login/login.js
+++ backend/routes/Login/login.js
@@ -1,19 +1,19 @@
-// var express = require("express");
-// var router = express.Router();
+var express = require("express");
+var router = express.Router();
 
-// const { db, auth } = require("../../firebase.js");
-// // const db = require("../../firebase.js");
-// const {
-//   getDocs,
-//   collection,
-//   query,
-//   where,
-//   doc,
-//   updateDoc,
-//   setDoc,
-//   addDoc,
-//   deleteDoc,
-// } = require("firebase/firestore");
+const { db, auth } = require("../../firebase.js");
+// const db = require("../../firebase.js");
+const {
+  getDocs,
+  collection,
+  query,
+  where,
+  doc,
+  updateDoc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+} = require("firebase/firestore");
 
 // let currentUser = {};
 // onAuthStateChanged(auth, (user) => {
@@ -78,27 +78,26 @@
 //   }
 // });
 
-// // // get all users
-// // router.get("/", async function (req, res) {
-// //   try {
-// //     const response = await getDocs(collection(db, "users"));
-// //     let ret = [];
-// //     response.forEach((doc) => ret.push({ ...doc.data(), id: doc.id }));
-// //     return res.status(200).json(ret);
-// //   } catch (error) {
-// //     return res.status(404).json(error);
-// //   }
-// // });
+// // get all users
+// router.get("/", async function (req, res) {
+//   try {
+//     const response = await getDocs(collection(db, "users"));
+//     let ret = [];
+//     response.forEach((doc) => ret.push({ ...doc.data(), id: doc.id }));
+//     return res.status(200).json(ret);
+//   } catch (error) {
+//     return res.status(404).json(error);
+//   }
+// });
 
-// // // add user
-// // router.post("/", async function (req, res) {
-// //   try {
-// //     const ref = await addDoc(collection(db, "products"), req.body);
-// //     console.log(ref.id);
-// //     return res.status(201).json({ message: "Post successful", id: ref.id });
-// //   } catch (error) {
-// //     return res.status(500).json(error);
-// //   }
-// // });
+// add user
+router.post("/", async function (req, res) {
+  try {
+    const ref = await addDoc(collection(db, "users"), req.body);
+    return res.status(201).json({ message: "Post successful", id: ref.id });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
